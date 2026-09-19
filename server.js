fcd911ccd647f5b0de0f150b7971ea1c17374817
rajaroy47 <rajaroy6297818984@gmail.com>
2026-09-19 13:37:36 +0530

    package json updated

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   family: 4,
 
